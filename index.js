f8de9378a1bba814779f59b9791cd0016e8da6b0
4cross <yeferok2@gmail.com>
2025-01-18 19:07:00 -0500

    server

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const http = require('http');
 const { onDocumentCreated } = require("firebase-functions/v2/firestore");
 const { notifyNewOrder } = require("./src/send_notifications");
 
@@ -17,5 +18,15 @@ exports.notifyNewOrder = onDocumentCreated(
 
         // Llamar a la función para enviar la notificación
         await notifyNewOrder(sellerId, orderData);
+        console.log("Notificación enviada para el vendedor:", sellerId);
     }
 );
+
+// Crear servidor HTTP para mantener el proceso activo
+const server = http.createServer((req, res) => {
+    res.end('Servidor está en funcionamiento');
+});
+
+server.listen(3000, () => {
+    console.log('Servidor está en funcionamiento en el puerto 3000');
+});
